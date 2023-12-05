f52cac5125c7299b9be3f95d4ac1c961f7ccd80d
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-12-05 11:24:30 +0100

    + book-rating

---
+++ book-rating/src/app/books/book/book.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Book } from '../shared/book';
+import { NgIf, CurrencyPipe } from '@angular/common';
+
+@Component({
+    selector: 'app-book',
+    standalone: true,
+    templateUrl: './book.component.html',
+    styleUrls: ['./book.component.scss'],
+    imports: [NgIf, CurrencyPipe]
+})
+export class BookComponent {
+
+  @Input() book?: Book;
+
+  @Output() rateUp = new EventEmitter<Book>();
+  @Output() rateDown = new EventEmitter<Book>();
+
+  doRateUp() {
+    this.rateUp.emit(this.book);
+  }
+
+  doRateDown() {
+    this.rateDown.emit(this.book);
+  }
+}
